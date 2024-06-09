f5461f7d1a6d2e37a30f09eeb5f1087f3188dc50
Damian Trafialek <damian.trafialek@gmail.com>
2024-06-09 02:03:21 +0200

    add: accessibilty for search bar and updated its styles

--- src/components/SearchStockBar.tsx
+++ src/components/SearchStockBar.tsx
@@ -24,24 +24,26 @@ function SearchBar(props: SearchBarProps) {
 		<div
 			className={`flex flex-col w-full mx-auto font-[sans-serif] ${className}`}
 		>
-			<p className="mb-4">
+			<label htmlFor="stock-search" className="mb-4">
 				In order to quickly filter the stocks, please type the name,
-				company or symbol of the stock you are looking for.
-			</p>
-			<div className="flex ">
+				company, or symbol of the stock you are looking for.
+			</label>
+			<div className="flex neumo-in-sm">
+				<label
+					htmlFor="stock-search"
+					className="flex items-center justify-center w-20"
+				>
+					<MagnifyingGlassSvg />
+				</label>
 				<input
+					id="stock-search"
 					type="text"
 					onChange={handleChange}
 					value={value}
 					placeholder="Stock name, company or symbol"
-					className="w-full outline-none text-sm px-4 py-3 neumo neumo-in-sm"
+					className="w-full outline-none text-sm pr-4 py-3 bg-transparent"
+					aria-label="Stock search"
 				/>
-				<button
-					type="button"
-					className="flex items-center justify-center px-5 neumo-out neumo-interactive"
-				>
-					<MagnifyingGlassSvg />
-				</button>
 			</div>
 		</div>
 	);
