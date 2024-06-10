434d564f826d4a2debb74e2456b023a05299280c
Damian Trafialek <damian.trafialek@gmail.com>
2024-06-10 23:50:54 +0200

    add: closing navbar menu on click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -93,6 +93,9 @@ function Navbar() {
 												key={item.name}
 												to={item.href}
 												className="highlight-underline -mx-3 block rounded-md px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+												onClick={() =>
+													setMobileMenuOpen(false)
+												}
 											>
 												{item.name}
 											</NavLink>
