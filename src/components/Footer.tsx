213d4a2b02e3cd12afba99162b0db94deb64881f
Damian Trafialek <damian.trafialek@gmail.com>
2024-06-11 20:18:48 +0200

    add: footer md screen

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
 	return (
-		<footer className="flex flex-col text-xs md:text-sm md:flex-row p-4 neumo-in-sm rounded-none mt-8">
+		<footer className="flex flex-col text-xs md:text-sm md:flex-row md:justify-evenly p-4 neumo-in-sm rounded-none mt-8">
 			<p>© 2024, All Rights Reserved</p>
 			<p className="pt-2 md:pt-0">Made with ❤️ by the Damian Trafiałek</p>
 
